d3d08193900167a3bb46406eaf1229e92ddea709
Mauro00r <131986203+Mauro00r@users.noreply.github.com>
2023-08-29 12:31:07 -0300

    Create Ej3_prog_ori_objet.cpp

---
+++ Ej3_prog_ori_objet.cpp
@@ -0,0 +1,80 @@
+using namespace std;
+#include <iostream>
+
+class punto {
+ private:
+ int x, y;
+ public:
+ punto();
+ 
+ punto(punto &p);
+ punto(int a, int b);
+ ~punto();
+ void cambio_cord(int a, int b);
+ int print_x();
+ int print_y();  
+ };
+ 
+//definicion de los metodos
+
+punto::punto(){ //constructor
+ x=0;
+ y=0;
+ }
+
+punto::punto(int a, int b){ //constructor
+ x=a;
+ y=b;
+ }
+ 
+ punto::punto(punto &p){ //constructor
+ x=p.x;
+ y=p.y;
+ }
+ 
+punto::~punto(){ //definicion
+cout<< "\nel objeto se destruye\n";
+} // es el equivalente a printf
+
+void punto::cambio_cord(int a, int b){
+x= a;
+y= b;
+}
+
+int punto::print_x(){
+return x;
+}
+
+int punto::print_y(){
+return y;
+}
+
+int main(){
+int a,b;
+class punto p, p1(10,20);
+cout<< "objeto p:";
+cout<< "\nla coordenada en x es: " << p.print_x();
+cout<< "\nla coordenada en y es: " << p.print_y();
+cout<< "\nobjeto p1:";
+cout<< "\nla coordenada en x es: " << p1.print_x();
+cout<< "\nla coordenada en y es: " << p1.print_y();
+cout<< "\nse cambia el estado del objeto p, ingresar coordenada x y luego y: ";
+cin>> a;
+cin>> b;
+p.cambio_cord(a,b);
+cout<< "\nse cambia el estado del objeto p1, ingresar coordenada x y luego y: ";
+cin>> a;
+cin>> b;
+p1.cambio_cord(a,b);
+
+punto p2(p1);
+
+cout<< "\nla nueva coordenada en x es: " << p.print_x();
+cout<< "\nla nueva coordenada en y es: " << p.print_y();
+cout<< "\nla nueva coordenada en x es: " << p1.print_x();
+cout<< "\nla nueva coordenada en y es: " << p1.print_y();
+cout<< "\nla comp en x de p2 es: "<< p2.print_x();
+cout<< "\nla comp en y de p2 es: "<< p2.print_y();
+
+return 0;
+}
